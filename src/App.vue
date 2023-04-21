80fda2c8c3ba47d279e76398aa9731fa705d49fe
KostasOiko <81381716+kostasoiko4@users.noreply.github.com>
2023-04-21 13:14:51 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="field is-grouped">
+    <p class="control is-expanded">
+      <input class="input" type="text" placeholder="Find a repository">
+    </p>
+    <p class="control">
+      <a class="button is-info">Search</a>
+    </p>
+  </div>
+</template>
+
+<style>
+@import 'bulma/css/bulma.min.css'
+</style>
